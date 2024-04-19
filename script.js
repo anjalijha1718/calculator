1afe11e05b6730a9c8280d9ee43f65e90dc611f4
anjalijha1718 <156492969+anjalijha1718@users.noreply.github.com>
2024-04-19 18:36:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let displayValue = '0';
+
+function updateDisplay() {
+  document.getElementById('display').innerText = displayValue;
+}
+
+function appendToDisplay(value) {
+  if (displayValue === '0') {
+    displayValue = value;
+  } else {
+    displayValue += value;
+  }
+  updateDisplay();
+}
+
+function clearDisplay() {
+  displayValue = '0';
+  updateDisplay();
+}
+
+function cancel() {
+  displayValue = displayValue.slice(0, -1);
+  if (displayValue === '') {
+    displayValue = '0';
+  }
+  updateDisplay();
+}
+
+function calculate() {
+  try {
+    displayValue = eval(displayValue);
+  } catch (error) {
+    displayValue = 'Error';
+  }
+  updateDisplay();
+}
